9336802d3b15a6efb40d67515723d13233256a47
Shivaranjan Dodavad <s>
2022-10-08 14:21:34 +0530

    oops class object demo

---
+++ src/CarCLassObjectDemo.java
@@ -0,0 +1,9 @@
+public class CarCLassObjectDemo {
+    public static void main(String args[]){
+        //creating object
+        CarClassDemo car =new CarClassDemo();
+        car.setSpeed(10);
+        System.out.println(car.getSpeed());
+
+    }
+}
---
+++ src/CarClassDemo.java
@@ -0,0 +1,15 @@
+public class CarClassDemo {
+    private String  doors;
+    private  String Engine;
+    private  String driver;
+    private  int speed;
+
+
+    public void setSpeed(int speed){
+        this.speed=speed;
+    }
+    public int getSpeed(){
+        return speed;
+    }
+
+}
