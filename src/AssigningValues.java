39ed5a785e4346d523047fdfd2df7f2966e88fa6
Shivaranjan Dodavad <s>
2022-11-09 13:46:01 +0530

    AssigningValues

---
+++ src/AssigningValues.java
@@ -0,0 +1,23 @@
+public class AssigningValues {
+    public static void main(String[] args) {
+        int var1;
+        var1=20;
+        int var2=20,var3=30;
+        int var4=var1+var2*var3;
+
+        System.out.println("var1 = "+var1);
+        System.out.println("var2 = "+var2);
+        System.out.println("var3 = "+var3);
+        System.out.println("var4 = "+var4);
+/*
+
+output
+"C:\Program Files\Java\jdk-16.0.2\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.2.2\lib\idea_rt.jar=62973:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2020.2.2\bin" -Dfile.encoding=UTF-8 -classpath "C:\Users\Shivaranjan Dodavad\IdeaProjects\JAVA SELF LEARNING\out\production\JAVA SELF LEARNING" AssigningValues
+var1 = 20
+var2 = 20
+var3 = 30
+var4 = 620
+ */
+
+    }
+}
