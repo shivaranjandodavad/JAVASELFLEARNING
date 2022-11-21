7ef37c846e051353d14e72b1a155fe160a9548c7
Shivaranjan Dodavad <s>
2022-11-21 14:29:17 +0530

    HelloYouClassObjectDemo

---
+++ src/Greet.java
@@ -0,0 +1,18 @@
+public class Greet{
+    public Greet(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("Hello "+name);
+    }
+}
---
+++ src/HelloYouClassObjectDemo.java
@@ -0,0 +1,8 @@
+public class HelloYouClassObjectDemo {
+    public static void main(String[] args) {
+     Greet g=new Greet("bob");
+     g.display();
+        Greet g2=new Greet("Shiv");
+        g2.display();
+    }
+}
