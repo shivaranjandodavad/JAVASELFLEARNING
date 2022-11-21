7c9f0ea6afb0b1a970f56236c8dcc3620bd03abf
Shivaranjan Dodavad <s>
2022-11-21 11:34:28 +0530

    SwitchDemo

---
+++ src/SwitchDemo.java
@@ -0,0 +1,17 @@
+public class SwitchDemo {
+    public static void main(String[] args) {
+        String n="abc";
+
+        switch (n){
+            case "abc":
+                System.out.print("abc");
+                break;
+            case "def":
+                System.out.print("def");
+                break;
+            default:
+                System.out.print("No match");
+
+        }
+    }
+}
