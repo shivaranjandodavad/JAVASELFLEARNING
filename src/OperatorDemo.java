043fbeaaa325b4fa9fe754c498cea9bed1908c2d
Shivaranjan Dodavad <s>
2022-11-21 10:44:27 +0530

    OperatorDemo

--- src/OperatorDemo.java
+++ src/OperatorDemo.java
@@ -1,13 +1,16 @@
 public class OperatorDemo {
     public static void main(String args[]){
-        int num1=10,num2=2;
+        int num1=11,num2=2;
         int sum=num1+num2;
         int diff=num1-num2;
         int mul=num1*num2;
-        int div=num1/num2;
+        double  div=(double)num1/num2;
+        int remi=num1%num2;
+
         System.out.println("sum ="+sum);
         System.out.println("diff ="+diff);
         System.out.println("mul ="+mul);
         System.out.println("division ="+div);
+        System.out.println("Reminder ="+remi);
     }
 }
