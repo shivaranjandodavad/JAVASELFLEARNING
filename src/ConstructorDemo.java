3490916a54151cc46fa0a54d617529e5d2dcd20d
Shivaranjan Dodavad <s>
2022-11-22 11:21:48 +0530

    MethodOverloading

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -5,12 +5,6 @@ class  Demo{
     public Demo(){
         i=5;
         j=10;
-
-    }
-}
-
-public class ConstructorDemo {
-    public static void main(String[] args) {
         Demo d =new Demo();
         System.out.println(d.i);
         System.out.println(d.j);
---
+++ src/MethodOverloading.java
@@ -0,0 +1,28 @@
+class Demos{
+    int num1;
+    int num2;
+    public static void area(int i ){
+        System.out.println("Area of the square = "+(i*i));
+
+    }
+    public static void area(int i ,int  b){
+        System.out.println("Area of the rectangle = " +(i*b));
+
+    }
+
+    public static void area(int i ,float  b){
+        System.out.println("Area of the triangle = "+(i*b));
+
+    }
+
+}
+
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Demos objDemo=new Demos();
+        objDemo.area(5);
+        objDemo.area(5,7);
+        objDemo.area(5,7.8f);
+    }
+}
