23a98a74d165379e3e20537c2ad54f41f8d6db24
Shivaranjan Dodavad <s>
2022-11-22 16:31:21 +0530

    Generic wildcard

---
+++ src/GenericWildcard.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericWildcard {
+    public static void viewData(List<?> list){
+        for(Object obj:list){
+            System.out.println(obj);
+        }
+    }
+    public static void integerElements(List<? super Integer> listitem){
+        listitem.add(100);
+    }
+    public static void main(String[] args) {
+
+        List<String> dataItems=new ArrayList<String>();
+        dataItems.add("data1");
+        dataItems.add("data2");
+        dataItems.add("data3");
+        viewData(dataItems);
+        /*
+        data1
+        data2
+        data3
+         */
+
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+        integerElements(arrayList);
+        viewData(arrayList);
+        //100
+
+    }
+}
--- src/TestGeneric.java
+++ src/TestGeneric.java
@@ -5,7 +5,7 @@ public class TestGeneric {
         ArrayList<Integer> arrayList=new ArrayList<Integer>();
         arrayList.add(10);
         arrayList.add(20);
-        arrayList.add("aaajdjd");//type safety according the arraylist it will allowonly Integer data it will enable type safety
+        //arrayList.add("aaajdjd");//type safety according the arraylist it will allowonly Integer data it will enable type safett
 
     }
 }
