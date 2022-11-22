6e0932c2afce388d34c2128275e47403740c4e10
Shivaranjan Dodavad <s>
2022-11-22 16:05:52 +0530

    Test Generic

--- src/AutoboxingUnboxingDemo.java
+++ src/AutoboxingUnboxingDemo.java
@@ -1,8 +1,8 @@
 public class AutoboxingUnboxingDemo {
     public static void main(String[] args) {
         int n1=20;
-        Integer I=n1;//autoboxing
-        int i=I;//unboxing
+        Integer I=n1;//autoboxing primitve to object type
+        int i=I;//unboxing object type to primitive type
 
         System.out.println("I : "+I);
         System.out.println("i : "+i);
---
+++ src/TestGeneric.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TestGeneric {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList=new ArrayList<Integer>();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add("aaajdjd");//type safety according the arraylist it will allowonly Integer data it will enable type safety
+
+    }
+}
